69d14585f703762e470421e4cf480a0ef8a467c4
gg8265 <f22512530@yahoo.com.tw>
2020-07-01 14:23:17 +0800

    自動匯入異警資料表功能

---
+++ ExcelImport/ExcelImport/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+
+
+namespace ExcelImport
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<List<ICell>> dataList = new List<List<ICell>>();
+        _Core _C;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _C = new _Core();
+            if (!_C.IsExist("alarmlist"))
+            {
+                CreateDTRemind.Text = "請先建立資料表";
+                CreateDTBtn.IsEnabled = true;
+                fileLoad.IsEnabled = false;
+            }
+            else
+            {
+                CreateDTBtn.IsEnabled = false;
+                fileLoad.IsEnabled = true;
+                CreateDTRemind.Text = "資料表已存在";
+            }
+        }
+        
+        private void CreateDTBtn_Click(object sender, RoutedEventArgs e)
+        {
+            _C.CreateTable("alarmlist");
+            CreateDTRemind.Text = "資料表建立成功";
+        }
+
+        private void FileImport_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dialog = new Microsoft.Win32.OpenFileDialog();
+            dialog.Title = "開啟檔案";
+            dialog.Filter = "xlsx file (*.*)|*.xlsx";
+            if (dialog.ShowDialog() == true)
+            {
+                dataList.Clear();
+                dataPath.Text = dialog.FileName;
+                using (FileStream stream = new FileStream(dialog.FileName, FileMode.Open))
+                {
+                    XSSFWorkbook workbook = new XSSFWorkbook(stream);
+                    for (int i = 0; i < workbook.NumberOfSheets; i++)
+                    {
+                        string sheetName = workbook.GetSheetName(i);
+                        XSSFSheet sheet = (XSSFSheet)workbook.GetSheetAt(i);
+                        for (int j = 1; j <= sheet.LastRowNum; j++)
+                        {
+                            IRow headerRow = sheet.GetRow(j);
+                            Tuple<string, string> engResult = alarmMsgSplit(headerRow.Cells[0].StringCellValue);
+                            Tuple<string, string> chtResult = alarmMsgSplit(headerRow.Cells[1].StringCellValue);
+                            bool IsSend = headerRow.Cells[2].StringCellValue == "Y" ? true : false;
+                            _C.InsertRow(engResult.Item1, chtResult.Item2, engResult.Item2, IsSend, sheetName);
+                            InsertOverflow.Text += $"{engResult.Item1}, {chtResult.Item2}, {engResult.Item2}, {IsSend}, {sheetName} \r\n";
+                        }
+                    }
+                    workbook.Close();
+                    InsertOverflow.Text += "Finish!!";
+                }
+            }
+        }
+
+        private Tuple<string, string> alarmMsgSplit(string str)
+        {
+            Tuple<string, string> tuple = null;
+            try
+            {
+                string[] splitString = Regex.Split(str, ",");
+                if(splitString.Length == 4)
+                {
+                    splitString[3] = splitString[3].Remove(0, splitString[2].Length).Trim();
+                }
+                else if(splitString.Length > 4)
+                {
+                    splitString[3] = splitString[3].Remove(0, splitString[2].Length).Trim();
+
+                    int index = splitString.Length - 3;
+                    string[] msg = new string[index];
+                    Array.Copy(splitString, 3, msg, 0, splitString.Length - 3);
+                    splitString[3] = string.Join(",", msg);
+                }
+                else if(splitString.Length < 4)
+                {
+                    tuple = new Tuple<string, string>(string.Empty, string.Empty);
+                    return tuple;
+                }
+                tuple = new Tuple<string, string>(splitString[2], splitString[3]);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"alarmMsgSplit error : {e.ToString()}");
+            }
+            return tuple;
+        }
+    }
+}
---
+++ ExcelImport/ExcelImport/_Core.cs
@@ -0,0 +1,136 @@
+﻿using Dapper;
+using NPOI.SS.UserModel;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelImport
+{
+    public class _Core
+    {
+        string strConn = string.Empty;
+        public _Core()
+        {
+            strConn = "server=127.0.0.1;uid=hartford;pwd=51321357;database=ds;SslMode=None;";
+            m_connection = new MySql.Data.MySqlClient.MySqlConnection(strConn);
+
+            if (m_connection.State != ConnectionState.Open)
+                m_connection.Open();
+        }
+
+        private bool IsConnected
+        {
+            get { return m_connection_p.State == ConnectionState.Open; }
+        }
+
+        private MySql.Data.MySqlClient.MySqlConnection m_connection_p;
+        private MySql.Data.MySqlClient.MySqlConnection m_connection
+        {
+            get
+            {
+                if (IsConnected && m_connection_p.Ping())
+                    return m_connection_p;
+                else
+                {
+                    m_connection_p = new MySql.Data.MySqlClient.MySqlConnection(strConn);
+                    m_connection_p.Open();
+                    return m_connection_p;
+                }
+            }
+            set
+            {
+                m_connection_p = value;
+            }
+        }
+
+        public bool CreateTable(string tableName)
+        {
+            string command = @"CREATE TABLE `hartnet`.`alarmlist` (
+                               `No` INT NOT NULL AUTO_INCREMENT,
+                               `alarmNum` VARCHAR(45) NULL,
+                               `alarmMsg_cht` VARCHAR(200) NULL,
+                               `alarmMsg_eng` VARCHAR(200) NULL,
+                               `IsSend` VARCHAR(10) NOT NULL,
+                               `version` VARCHAR(45) NOT NULL,
+                               PRIMARY KEY (`No`),
+                               UNIQUE INDEX `No_UNIQUE` (`No` ASC));";
+            return SetData(command, new { });
+        }
+
+        public bool IsExist(String tableName)
+        {
+            String sqlcommand = @"show tables from `hartnet` where `tables_in_hartnet` = '" + tableName + @"';";
+            bool IsCorrect = false;
+            IsCorrect = GetData(sqlcommand, new { }, out List<string> dt);
+
+            //資料正確擷取，而且資料列的數列為1筆
+            return IsCorrect && dt.Count == 1;
+        }
+
+        public bool InsertRow(string alarmNum, string alarmMsg_cht, string alarmMsg_eng, bool IsSend, string version)
+        {
+            bool IsCorrect = false;
+            try
+            {
+                string command = @"INSERT INTO `hartnet`.`alarmlist` (`alarmNum`, `alarmMsg_cht`, `alarmMsg_eng`,
+                                                                      `IsSend`, `version`) VALUES (
+                                                                      @alarmNum, @alarmMsg_cht, @alarmMsg_eng,
+                                                                      @IsSend, @version);";
+                IsCorrect = SetData(command, new
+                            {
+                                alarmNum,
+                                alarmMsg_cht,
+                                alarmMsg_eng,
+                                IsSend,
+                                version
+                            });
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            return IsCorrect;
+        }
+
+        public bool GetData<T>(string command, object data, out List<T> result)
+        {
+            bool IsCorrect = false;
+            result = null;
+            try
+            {
+                if (IsConnected)
+                {
+                    result = m_connection.Query<T>(command, data).ToList();
+                    if (result.Count > 0)
+                        IsCorrect = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            return IsCorrect;
+        }
+
+        public bool SetData(string command, object data)
+        {
+            bool IsCorrect = false;
+            try
+            {
+                if (IsConnected)
+                {
+                    int s = m_connection.Execute(command, data);
+                    IsCorrect = s > 0 ? true : false;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            return IsCorrect;
+        }
+    }
+}
